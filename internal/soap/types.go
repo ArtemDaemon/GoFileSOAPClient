191cc5182b26621833317411e86a439488da5908
ArtemDaemon <artem.daemon.official@gmail.com>
2025-06-09 12:35:41 +0700

    Убрал лишние типы данных

--- internal/soap/types.go
+++ internal/soap/types.go
@@ -2,7 +2,6 @@ package soap
 
 import (
 	"encoding/xml"
-	"net/http"
 )
 
 type SOAPEnvelope struct {
@@ -17,37 +16,4 @@ type SOAPBody struct {
 type UploadJSONRequest struct {
 	Filename string `xml:"Filename"`
 	Content  string `xml:"Content"`
-}
-
-type SOAPResponseEnvelope struct {
-	XMLName xml.Name         `xml:"Envelope"`
-	Xmlns   string           `xml:"xmlns:soap,attr"`
-	Body    SOAPResponseBody `xml:"Body"`
-}
-
-type SOAPResponseBody struct {
-	UploadJSONResponse UploadJSONResponse `xml:"UploadJSONResponse"`
-}
-
-type UploadJSONResponse struct {
-	Status  string `xml:"Status"`
-	Message string `xml:"Message"`
-}
-
-func UnmarshalEnvelope(data []byte, v interface{}) error {
-	return xml.Unmarshal(data, v)
-}
-
-func WriteSOAPResponse(w http.ResponseWriter, status, message string) {
-	response := SOAPResponseEnvelope{
-		Xmlns: "http://schemas.xmlsoap.org/soap/envelope/",
-		Body: SOAPResponseBody{
-			UploadJSONResponse: UploadJSONResponse{
-				Status:  status,
-				Message: message,
-			},
-		},
-	}
-	w.Header().Set("Content-Type", "text/xml; charset=utf-8")
-	xml.NewEncoder(w).Encode(response)
 }
